d0d0d5a171458c7991a97fcaaa9306cfc0198f1e
Kordyss99 <kordyss99@outlook.com>
2025-04-03 23:51:24 +0300

    yas-hesaplama

---
+++ yas-hesaplama/yas-hesaplama/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace yas_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Doğum Yılınızı Giriniz: ");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            int yashesap = 2025 - yas;
+
+            Console.WriteLine($"Yaşınız : {yashesap}");
+            Console.ReadLine();
+        }
+    }
+}
