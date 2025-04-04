c3b0debf7361ffc462b992b08e59deef56fd6e57
Kordyss99 <kordyss99@outlook.com>
2025-04-04 23:38:21 +0300

    fibonacci

---
+++ fibonaci/fibonaci/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace fibonaci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = 0, s2 = 1, fib;
+            for (int s3 = 0; s3 < 20; s3++)
+            {
+                Console.WriteLine(s1 + " ");
+                fib = s1 + s2;
+                s1 = s2;
+                s2 = fib;
+            }
+            
+        }
+    }
+}
