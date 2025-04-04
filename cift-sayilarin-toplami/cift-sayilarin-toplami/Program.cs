dd5b56846a5b50cf345895dfcaec058406086027
Kordyss99 <kordyss99@outlook.com>
2025-04-05 00:12:19 +0300

    Cift Sayıların Toplamı

---
+++ cift-sayilarin-toplami/cift-sayilarin-toplami/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace cift_sayilarin_toplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam=0;
+            for (int i = 0; i <= 30; i += 2)
+            {
+                toplam += i;
+
+            }
+            Console.WriteLine($"Toplam {toplam}");
+        }
+    }
+}
