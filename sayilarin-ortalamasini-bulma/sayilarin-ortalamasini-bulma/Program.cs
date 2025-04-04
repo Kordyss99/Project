c693d36312a0205b00f94c0796b2b31328c8cb09
Kordyss99 <kordyss99@outlook.com>
2025-04-05 00:22:35 +0300

    10 adet girilen sayının ortlaması

---
+++ sayilarin-ortalamasini-bulma/sayilarin-ortalamasini-bulma/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace sayilarin_ortalamasini_bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            double bolme = 0;
+            for (int i = 1; i <= 30; i++)
+            {
+                toplam += i;
+            }
+            bolme = toplam / 30;
+            Console.WriteLine(bolme);
+            Console.ReadLine();
+        }
+    }
+}
