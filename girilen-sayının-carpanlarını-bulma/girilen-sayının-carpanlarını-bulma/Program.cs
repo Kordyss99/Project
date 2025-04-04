815988c5fd83311390b122de201392843d1bc92f
Kordyss99 <kordyss99@outlook.com>
2025-04-04 23:54:42 +0300

    Girilen Sayının carpanlarını bulma

---
+++ girilen-sayının-carpanlarını-bulma/girilen-sayının-carpanlarını-bulma/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace girilen_sayının_carpanlarını_bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz: ");
+            int say = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Bu sayının çarpanları: ");
+
+            for (int i = 1; i<=say; i++)
+            {
+                if (say % i == 0) 
+                {
+                    Console.Write(i + " - ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
